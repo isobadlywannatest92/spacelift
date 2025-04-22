0f71b05e81eb8c3e5c69135f77b28798c85743c1
isobadlywannatest92 <142501062+isobadlywannatest92@users.noreply.github.com>
2025-04-22 13:28:40 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,7 @@
-output "bucket_name" {
-  description = "The name of the created S3 bucket"
-  value       = aws_s3_bucket.example.bucket
-}
-
-output "bucket_arn" {
-  description = "The ARN of the created S3 bucket"
-  value       = aws_s3_bucket.example.arn
+output "debug_bucket_tags" {
+  value = {
+    Environment = var.environment
+    Owner       = var.owner
+    Project     = "spacelift-demo"
+  }
 }
