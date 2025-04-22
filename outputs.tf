27682ac769982d10f16486c1dba3e9e7c2f7e5fd
isobadlywannatest92 <142501062+isobadlywannatest92@users.noreply.github.com>
2025-04-22 13:39:07 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,9 @@
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
+output "bucket_name" {
+  description = "The name of the created S3 bucket"
+  value       = aws_s3_bucket.example.bucket
 }
 
-variable "bucket_prefix" {
-  description = "Prefix for bucket name"
-  type        = string
-  default     = "spacelift-clean"
-}
-
-variable "environment" {
-  description = "Environment label"
-  type        = string
-  default     = "dev"
-}
-
-variable "owner" {
-  description = "Bucket owner"
-  type        = string
-  default     = "weronika"
-}
-
-variable "project" {
-  description = "Project name for tagging"
-  type        = string
-  default     = "spacelift-demo"
+output "bucket_arn" {
+  description = "The ARN of the created S3 bucket"
+  value       = aws_s3_bucket.example.arn
 }
